6a98523ff569e149146c5e7ebaeae4c251c1c031
Brayan <btellez948@gmail.com>
2025-07-11 18:37:41 -0600

    [Corrección] Corrección del mensaje de actualización de usuario.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -116,7 +116,7 @@ adminRouter.patch('/users/:id', async (req, res) => {
 
   try {
     const user = await AdminService.updateUser(id, data.data);
-    res.status(200).json({ message: 'Usuario creado de manera exitosa', user });
+    res.status(200).json({ message: 'Usuario actualizado de manera exitosa', user });
   } catch(err) {
     console.log(err);
     res.status(403).json({ message: 'Acceso denegado' });
